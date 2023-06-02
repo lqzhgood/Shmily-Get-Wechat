0bfbe1d36ce1bde3f74ec71f5e935b4c94ff69af
lqzh <9134671+lqzhgood@users.noreply.github.com>
2023-06-02 23:35:27 +0800

    🐞 fix(dbToJson): JAVA 环境变量 最大运行内存

--- export/db-android/2 dbToJson/index.js
+++ export/db-android/2 dbToJson/index.js
@@ -1,20 +1,29 @@
-const _ = require('lodash');
-const fs = require('fs-extra');
-const path = require('path');
-const exec = require('child_process').execSync;
+const _ = require("lodash");
+const fs = require("fs-extra");
+const path = require("path");
+const exec = require("child_process").execSync;
 
-const { FILTER_USER } = require('./config.js');
+const { FILTER_USER } = require("./config.js");
 
-console.log('正在解密 Message');
-fs.copyFileSync('./lib/decodeMessageTable/Releases/decodeMessageTable.exe', './input/decodeMessageTable.exe');
-exec('decodeMessageTable.exe', { cwd: path.join(__dirname, './input/'), encoding: 'utf8' });
-fs.removeSync('./input/decodeMessageTable.exe');
+console.log("正在解密 Message");
+fs.copyFileSync(
+    "./lib/decodeMessageTable/Releases/decodeMessageTable.exe",
+    "./input/decodeMessageTable.exe"
+);
+exec("decodeMessageTable.exe", {
+    cwd: path.join(__dirname, "./input/"),
+    encoding: "utf8",
+    env: { JAVA_OPTS: "-Xmx4096m" },
+});
+fs.removeSync("./input/decodeMessageTable.exe");
 
-console.log('FILTER_USER', FILTER_USER);
-fs.mkdirpSync('./dist/BLOB/');
+console.log("FILTER_USER", FILTER_USER);
+fs.mkdirpSync("./dist/BLOB/");
 
-const msg = require('./input/message.json').filter(v => v.talker == FILTER_USER);
-const res = msg.map(v => {
+const msg = require("./input/message.json").filter(
+    (v) => v.talker == FILTER_USER
+);
+const res = msg.map((v) => {
     const { msgId } = v;
 
     const lvbufferFile = `./input/BLOB/${msgId}-lvbuffer`;
@@ -22,13 +31,13 @@ const res = msg.map(v => {
         fs.copySync(lvbufferFile, `./dist/BLOB/${msgId}-lvbuffer`);
     }
 
-    v.hasXml.forEach(k => {
+    v.hasXml.forEach((k) => {
         const kv = Object.create(null);
 
         for (const key in v[k]) {
             if (Object.hasOwnProperty.call(v[k], key)) {
                 const value = v[k][key];
-                const _key = key.replace(/^\.msg/, 'msg');
+                const _key = key.replace(/^\.msg/, "msg");
                 _.set(kv, _key, value);
             }
         }
@@ -37,11 +46,14 @@ const res = msg.map(v => {
 
     return v;
 });
-console.log('msg length', res.length);
-fs.writeFileSync('./dist/db-android_msg_res.json', JSON.stringify(res, null, 4));
+console.log("msg length", res.length);
+fs.writeFileSync(
+    "./dist/db-android_msg_res.json",
+    JSON.stringify(res, null, 4)
+);
 
-require('./db/index.js');
+require("./db/index.js");
 
-console.log('ok');
+console.log("ok");
 
 // setTimeout(() => {}, 10000000);
