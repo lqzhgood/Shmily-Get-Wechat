a1a32af218b2385719b94efaf36c779d06882ee1
lqzh <9134671+lqzhgood@users.noreply.github.com>
2023-12-10 00:36:57 +0800

    🐞 fix(assetsFileMd5): 修改描述信息

--- ToMsg/lib/assetsFileMd5.js
+++ ToMsg/lib/assetsFileMd5.js
@@ -1,13 +1,10 @@
-const fs = require("fs");
+const fs = require('fs');
 
-const { ASSETS_ROOT_DIR } = require("../config");
-const { getDirMd5 } = require("../utils/index");
+const { ASSETS_ROOT_DIR } = require('../config');
+const { getDirMd5 } = require('../utils/index');
 
 (async () => {
     const md5Json = await getDirMd5(ASSETS_ROOT_DIR);
-    fs.writeFileSync(
-        "./dist/_temp/ASSET_FILES.json",
-        JSON.stringify(md5Json, null, 4)
-    );
-    console.log("ok", md5Json.length);
+    fs.writeFileSync('./dist/_temp/ASSET_FILES.json', JSON.stringify(md5Json, null, 4));
+    console.log('md5 文件数', md5Json.length);
 })();
